c6a34f87517138048e661084251157f712848f6f
swlws <979623074@qq.com>
2018-03-08 20:24:01 +0800

    log

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 'use static'
-
+let fs = require('fs');
+let path = require('path');
 let express = require('express');
+let morgan = require('morgan');
+let rfs = require('rotating-file-stream');
+
 let app = express();
+let logDirectory = path.join(__dirname, 'log');
+
+// ensure log directory exists
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+
+// create a rotating write stream
+var accessLogStream = rfs('access.log', {
+  interval: '1d', // rotate daily
+  path: logDirectory
+})
+
+// setup the logger
+app.use(morgan('combined', {stream: accessLogStream}))
 
 app.use(express.static('public'));
 
